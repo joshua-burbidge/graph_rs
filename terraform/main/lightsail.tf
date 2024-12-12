3972393d9d514a40ec0eab132a006b314bac583b
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-12-12 06:33:09 -0600

    enable deployment

--- terraform/main/lightsail.tf
+++ terraform/main/lightsail.tf
@@ -11,28 +11,28 @@ resource "aws_lightsail_container_service" "container_service" {
   }
 }
 
-# resource "aws_lightsail_container_service_deployment_version" "deployment" {
-#   service_name = aws_lightsail_container_service.container_service.name
+resource "aws_lightsail_container_service_deployment_version" "deployment" {
+  service_name = aws_lightsail_container_service.container_service.name
 
-#   container {
-#     container_name = "main-container"
-#     image          = "${data.aws_ecr_repository.graph_rs_repo.repository_url}:${var.git_commit_sha}"
-#     ports = {
-#       80 = "HTTP"
-#     }
-#     command     = []
-#     environment = {}
-#   }
+  container {
+    container_name = "main-container"
+    image          = "${data.aws_ecr_repository.graph_rs_repo.repository_url}:${var.git_commit_sha}"
+    ports = {
+      80 = "HTTP"
+    }
+    command     = []
+    environment = {}
+  }
 
-#   public_endpoint {
-#     container_name = "main-container"
-#     container_port = 80 # Exposed port
-#     health_check {
-#       healthy_threshold   = 2
-#       unhealthy_threshold = 2
-#     }
-#   }
-# }
+  public_endpoint {
+    container_name = "main-container"
+    container_port = 80 # Exposed port
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+    }
+  }
+}
 
 data "aws_iam_policy_document" "aws_ecr_repository_policy" {
   statement {
