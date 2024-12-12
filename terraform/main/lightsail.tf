681f03b30fc3bab701b18724bcaf5232e7e76b77
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-12-12 06:54:14 -0600

    remove dpeloyment

--- terraform/main/lightsail.tf
+++ terraform/main/lightsail.tf
@@ -15,28 +15,28 @@ output "lightsail_role" {
   value = aws_lightsail_container_service.container_service.private_registry_access[0].ecr_image_puller_role[0].principal_arn
 }
 
-resource "aws_lightsail_container_service_deployment_version" "deployment" {
-  service_name = aws_lightsail_container_service.container_service.name
+# resource "aws_lightsail_container_service_deployment_version" "deployment" {
+#   service_name = aws_lightsail_container_service.container_service.name
 
-  container {
-    container_name = "main-container"
-    image          = "${data.aws_ecr_repository.graph_rs_repo.repository_url}:${var.git_commit_sha}"
-    ports = {
-      80 = "HTTP"
-    }
-    command     = []
-    environment = {}
-  }
+#   container {
+#     container_name = "main-container"
+#     image          = "${data.aws_ecr_repository.graph_rs_repo.repository_url}:${var.git_commit_sha}"
+#     ports = {
+#       80 = "HTTP"
+#     }
+#     command     = []
+#     environment = {}
+#   }
 
-  public_endpoint {
-    container_name = "main-container"
-    container_port = 80 # Exposed port
-    health_check {
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-    }
-  }
-}
+#   public_endpoint {
+#     container_name = "main-container"
+#     container_port = 80 # Exposed port
+#     health_check {
+#       healthy_threshold   = 2
+#       unhealthy_threshold = 2
+#     }
+#   }
+# }
 
 data "aws_iam_policy_document" "aws_ecr_repository_policy" {
   statement {
@@ -49,10 +49,6 @@ data "aws_iam_policy_document" "aws_ecr_repository_policy" {
         "arn:aws:iam::575737149124:role/graph-rs-deploy",
         # aws_lightsail_container_service.container_service.private_registry_access[0].ecr_image_puller_role[0].principal_arn
       ]
-    }
-    principals {
-      type        = "Service"
-      identifiers = ["lightsail.amazonaws.com"]
     }
   }
 }
