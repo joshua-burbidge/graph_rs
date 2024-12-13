d318abdcd78b0328aff9d86207d6964ccc8c937e
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-12-12 21:25:25 -0600

    clean up

--- terraform/ecr/vars.tf
+++
@@ -1,5 +0,0 @@
-variable "ecr_repository_name" {
-  type        = string
-  default     = "graph-rs-repository"
-  description = "name of repository to create"
-}
--- terraform/main/lightsail.tf
+++ terraform/main/lightsail.tf
@@ -1,7 +1,7 @@
 resource "aws_lightsail_container_service" "container_service" {
   name        = "graph-rs-container-service"
   power       = "nano" # Available options: nano, micro, small, medium, large, xlarge
-  scale       = 1      # Number of containers to run
+  scale       = 1
   is_disabled = false
 
   private_registry_access {
@@ -14,7 +14,6 @@ resource "aws_lightsail_container_service" "container_service" {
 output "lightsail_role" {
   value = aws_lightsail_container_service.container_service.private_registry_access[0].ecr_image_puller_role[0].principal_arn
 }
-# this works
 
 resource "aws_lightsail_container_service_deployment_version" "deployment" {
   service_name = aws_lightsail_container_service.container_service.name
@@ -37,24 +36,4 @@ resource "aws_lightsail_container_service_deployment_version" "deployment" {
       unhealthy_threshold = 2
     }
   }
-}
-
-data "aws_iam_policy_document" "aws_ecr_repository_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["ecr:*"]
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::575737149124:user/admin",
-        "arn:aws:iam::575737149124:role/graph-rs-deploy",
-        aws_lightsail_container_service.container_service.private_registry_access[0].ecr_image_puller_role[0].principal_arn
-      ]
-    }
-  }
-}
-
-resource "aws_ecr_repository_policy" "policy" {
-  repository = aws_ecr_repository.graph_rs_repo.name
-  policy     = data.aws_iam_policy_document.aws_ecr_repository_policy.json
 }
